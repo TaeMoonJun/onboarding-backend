f76c249ba83ba34abde6e3007fe95bcf38992412
Taejun Moon <junall10022@gmail.com>
2023-08-22 00:45:26 +0900

    feat: Add Auth DTO

---
+++ src/main/java/com/example/onboarding/DTOs/AuthDTO.java
@@ -0,0 +1,22 @@
+package com.example.onboarding.DTOs;
+
+import com.example.onboarding.entities.Authority;
+import com.example.onboarding.entities.User;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Builder
+@Data
+public class AuthDTO {
+    private String email;
+    private String password;
+
+    public User toEntity(PasswordEncoder passwordEncoder){
+        return User.builder()
+                .email(email)
+                .password(passwordEncoder.encode(password))
+                .authority(Authority.ROLE_USER)
+                .build();
+    }
+}
