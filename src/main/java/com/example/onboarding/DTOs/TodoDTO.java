6706bdb48cdb952f7eafe5799948e33efcd7699b
Taejun Moon <junall10022@gmail.com>
2023-08-22 21:38:58 +0900

    feat: Add Todo DTO

---
+++ src/main/java/com/example/onboarding/DTOs/TodoDTO.java
@@ -0,0 +1,21 @@
+package com.example.onboarding.DTOs;
+
+import com.example.onboarding.entities.Todo;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class TodoDTO {
+    private String todo;
+
+    public Todo toEntity() {
+        return Todo.builder()
+                .content(todo)
+                .build();
+    }
+}
