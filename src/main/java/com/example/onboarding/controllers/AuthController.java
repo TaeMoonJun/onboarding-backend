4a2f8d5c83d4bcb0ad6ca77f49f8d0df3ba8cf4a
Taejun Moon <junall10022@gmail.com>
2023-08-22 21:31:17 +0900

    feat: Add auth controller and registerUserAccount method

---
+++ src/main/java/com/example/onboarding/controllers/AuthController.java
@@ -0,0 +1,26 @@
+package com.example.onboarding.controllers;
+
+import com.example.onboarding.DTOs.AuthDTO;
+import com.example.onboarding.DTOs.TokenDTO;
+import com.example.onboarding.services.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/auth")
+@AllArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthDTO> registerUserAccount(@RequestBody @Valid AuthDTO authDTO){
+        return ResponseEntity.ok(authService.registerNewUserAccount(authDTO));
+    }
+
+}
