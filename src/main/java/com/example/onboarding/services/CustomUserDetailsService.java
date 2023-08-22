2b4d799c25f70e101d08d0742d8adbeb1b7de9e9
Taejun Moon <junall10022@gmail.com>
2023-08-22 21:18:44 +0900

    feat: Add Custom UserDetails service for auth

---
+++ src/main/java/com/example/onboarding/services/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.example.onboarding.services;
+
+import com.example.onboarding.entities.User;
+import com.example.onboarding.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByEmail(username)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException(username + " 을 DB에서 찾을 수 없습니다"));
+    }
+
+    private UserDetails createUserDetails(User user) {
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(user.getAuthority().toString());
+
+        return new org.springframework.security.core.userdetails.User(
+                String.valueOf(user.getId()),
+                user.getPassword(),
+                Collections.singleton(grantedAuthority)
+        );
+    }
+}
